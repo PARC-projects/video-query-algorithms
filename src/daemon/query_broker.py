36c459f377db46ea54fb3dda95b29bff2d3cae61
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-20 11:27:11 -0600

    Format logger

--- src/daemon/query_broker.py
+++ src/daemon/query_broker.py
@@ -7,13 +7,20 @@ import threading
 import logging
 from datetime import datetime
 
+LOG_NAME = 'logs/query_broker_{0}.log'.format(
+    datetime.now().strftime("%Y_%m_%d"))
 
-logging.basicConfig(filename='logs/query_broker_{0}.log'.format(datetime.now().strftime("%Y_%m_%d")),level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s; %(levelname)s; {%(module)s}; [%(funcName)s] %(message)s',
+    filename=LOG_NAME,
+    level=logging.DEBUG)
+
 
 def hello_world():
-    threading.Timer(2.0, hello_world).start() # called every minute
+    threading.Timer(2.0, hello_world).start()  # called every 2 seconds
     logging.debug('This message should go to the log file')
     logging.info('So should this')
     logging.warning('And this, too')
+
 
 hello_world()
