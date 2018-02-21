234101d12fa6aa0ddb32ffb0545707b99b426916
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-21 13:55:43 -0600

    Update status code logging

--- src/daemon/query-broker/status.py
+++ src/daemon/query-broker/status.py
@@ -9,20 +9,11 @@ BASE_URL = "http://127.0.0.1:8000/"
 
 
 class QueryStatus():
-    """
-    Request queries that meet processing_state requirements
-    """
-
     def getStatus(self):
         """
         Request queries that meet processing_state requirements
 
         Returns:
-            {
-                compute_similarity: <Query>,
-                optimize_weight: <Query>,
-                compute_new_matches: <Query>
-            }
         """
 
         return {
@@ -47,11 +38,10 @@ class QueryStatus():
 
     def _makeRequest(self, url):
         try:
-            response = requests.get('https://github.com/timeline.json')
+            response = requests.get(url)
             if response.status_code == requests.codes.ok:
                 return response.json()
             else:
-                logger.info(response.status_code)
                 return None
         except requests.exceptions.HTTPError as errh:
             logger.error("Http Error: " + errh)
