4cc7f53c5601fe970874672b58c3d8a153ee68d5
fetorres <frank.home@sbcglobal.net>
2018-02-21 15:47:35 -0800

    add feature data files in SHRP2_Forward_clips_features directory so it can be used with load_db.py

--- create_db.py
+++ create_db.py
@@ -1,38 +1,38 @@
 import psycopg2
 
 '''
-This program loads the 'features' and supporting 'cnn_streams' tables into a postgres database named video-query.
+This program loads the 'features' and supporting 'dnn_streams' tables into a postgres database named video-query.
 Currently it works for a database of that name created locally by user torres.
 The SQL CREATE scripts are copied from pgAdmin 4.
 For now, we are assuming the deep net used to compute features is a TSN net trained on UCF-101 data, 
 and the ensemble averaging is over models trained on splits 1, 2, and 3.
 '''
 
-conn = psycopg2.connect("host=localhost dbname=video-query user=torres")  #creates Connection object
-cur = conn.cursor()   #  creates Cursor object for issuing commands
+conn = psycopg2.connect("host=localhost dbname=video-query user=torres")  # creates Connection object
+cur = conn.cursor()   # creates Cursor object for issuing commands
 
-# create cnn_streams table, a simple table with allowed names of stream/ensemble number options
+# create dnn_streams table, a simple table with allowed names of stream/ensemble number options
 cur.execute("""
-    CREATE TABLE public.cnn_streams
+    CREATE TABLE public.dnn_streams
     (
-        cnn_stream character varying(80) COLLATE pg_catalog."default" NOT NULL,
+        dnn_stream character varying(80) COLLATE pg_catalog."default" NOT NULL,
         max_number_of_splits smallint,
-        CONSTRAINT cnn_streams_cnn_stream_unique UNIQUE (cnn_stream)
+        CONSTRAINT dnn_streams_dnn_stream_unique UNIQUE (dnn_stream)
     )
     WITH (
         OIDS = FALSE
     )
     TABLESPACE pg_default;
     
-    COMMENT ON COLUMN public.cnn_streams.max_number_of_splits
+    COMMENT ON COLUMN public.dnn_streams.max_number_of_splits
         IS 'maximum allowed number of splits for this stream';
 """)
 # add names of stream/ensemble options
 cur.execute("""
-    INSERT INTO cnn_streams (cnn_stream, max_number_of_splits)
+    INSERT INTO dnn_streams (dnn_stream, max_number_of_splits)
     VALUES (%s, %s), (%s, %s), (%s, %s);
     """,
-    ['rgb', 3, 'optical flow', 3, 'warped_optical_flow', 3]
+            ['rgb', 3, 'optical flow', 3, 'warped_optical_flow', 3]
 )
 conn.commit()
 
@@ -42,22 +42,22 @@ cur.execute("""
     (
         id bigint NOT NULL DEFAULT nextval('features_id_seq'::regclass),
         video_clip_id bigint NOT NULL,
-        cnn_stream character varying COLLATE pg_catalog."default" NOT NULL,
-        cnn_stream_split smallint NOT NULL,
+        dnn_stream character varying COLLATE pg_catalog."default" NOT NULL,
+        dnn_stream_split smallint NOT NULL,
         feature_name character varying(80) COLLATE pg_catalog."default" NOT NULL,
-        cnn_weights_file_uri character varying(256) COLLATE pg_catalog."default" NOT NULL,
+        dnn_weights_file_uri character varying(256) COLLATE pg_catalog."default" NOT NULL,
         feature double precision[] NOT NULL,
         CONSTRAINT features_pkey PRIMARY KEY (id),
-        CONSTRAINT no_duplicate_feature_entries UNIQUE (video_clip_id, cnn_stream, cnn_stream_split, feature_name),
-        CONSTRAINT cnn_stream_fkey FOREIGN KEY (cnn_stream)
-            REFERENCES public.cnn_streams (cnn_stream) MATCH SIMPLE
+        CONSTRAINT no_duplicate_feature_entries UNIQUE (video_clip_id, dnn_stream, dnn_stream_split, feature_name),
+        CONSTRAINT cnn_stream_fkey FOREIGN KEY (dnn_stream)
+            REFERENCES public.dnn_streams (dnn_stream) MATCH SIMPLE
             ON UPDATE NO ACTION
             ON DELETE NO ACTION,
         CONSTRAINT video_clip_id_fkey FOREIGN KEY (video_clip_id)
             REFERENCES public.video_clips (id) MATCH SIMPLE
             ON UPDATE NO ACTION
             ON DELETE NO ACTION,
-        CONSTRAINT split_gt_0 CHECK (cnn_stream_split > 0) NOT VALID
+        CONSTRAINT split_gt_0 CHECK (dnn_stream_split > 0) NOT VALID
     )
     WITH (
         OIDS = FALSE
@@ -67,16 +67,16 @@ cur.execute("""
     COMMENT ON COLUMN public.features.video_clip_id
         IS 'id in the video_clips table for the clip corresponding to this row';
     
-    COMMENT ON COLUMN public.features.cnn_stream
+    COMMENT ON COLUMN public.features.dnn_stream
         IS 'cnn stream name from cnn_streams table signifying which if the streams in the multi-stream CNN this row belongs';
     
-    COMMENT ON COLUMN public.features.cnn_stream_split
+    COMMENT ON COLUMN public.features.dnn_stream_split
         IS 'split number of cnn_stream, ensemble averaging is over splits of a given cnn stream';
     
     COMMENT ON COLUMN public.features.feature_name
         IS 'should match the feature name in the CNN (e.g., caffe) model';
     
-    COMMENT ON COLUMN public.features.cnn_weights_file_uri
+    COMMENT ON COLUMN public.features.dnn_weights_file_uri
         IS 'location of the weights file for the CNN model used to compute the feature';
     
     COMMENT ON COLUMN public.features.feature
