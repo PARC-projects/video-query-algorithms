429c8f3c23a11f60a44669405c3b7eb3b758e362
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-03-11 19:23:01 -0500

    Broker call to compute matches

--- src/broker/broker.py
+++ src/broker/broker.py
@@ -6,8 +6,13 @@ TODO: Consider shifting to a daemonize approach to manage this process.
 """
 import threading
 import status
+import os
+import sys
 from datetime import datetime
 import logging
+
+sys.path.insert(0,os.path.join(os.getcwd(), os.pardir,  'models'))
+import compute_matches
 
 LOOP_EXECUTION_TIME = 10.0  # In seconds
 LOG_NAME = 'logs/query_broker_{0}.log'.format(
@@ -26,7 +31,9 @@ def main():
     queryStatus = status.QueryStatus()
     threading.Timer(LOOP_EXECUTION_TIME, main).start()
     try:
-        queryStatus.getStatus()
+        result = queryStatus.getStatus()
+        compute_matches.new_matches(result["compute_new_matches"])
+        compute_matches.revised_matches(result["compute_similarity"], [])
     except Exception as e:
         logging.error(e)
 
