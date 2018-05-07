1f3fd62d7e909cdfa5a7a20139340288b09ffc9b
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-07 15:59:44 -0500

    Setup recursive throughput

--- src/broker.py
+++ src/broker.py
@@ -5,9 +5,11 @@ This script is designed to be executed as a long running service.
 TODO: Consider shifting to a daemonize approach to manage this process.
 """
 import threading
+import logging
+import os
+import time
 from api.status import QueryStatus
 from datetime import datetime
-import logging
 from models import compute_matches
 
 LOOP_EXECUTION_TIME = 10.0  # In seconds
@@ -35,8 +37,24 @@ def main():
     except Exception as e:
         logging.error(e)
     finally:
-        # create a new thread
-        threading.Timer(LOOP_EXECUTION_TIME, main).start()
+        # TODO: Discuss options (thoughts)
+        # We can do a number of things in this process.
+        # - Thread so we have non-blocking operations. Some reasoning...
+        #   - We want to run compute processes in parallel.
+        #   - We want to run a bit of logic after we send data down-stream but
+        #       don't want to wait for down-stream processing to return.
+        # - Block operations until down-stream is done.
+        #   - Simple and traceable.
+        #   - Would we want to throttle the synchronous process?
+        #       - If not, there would be no value in setting a sleep to throttle.
+
+        # TODO: Is BROKER_IS_THREADED a good name?
+        if os.environ.get('BROKER_IS_THREADED') == 'True':
+            # create a new thread
+            threading.Timer(LOOP_EXECUTION_TIME, main).start()
+        else:
+            # time.sleep(LOOP_EXECUTION_TIME)
+            main()
 
 
 if __name__ == '__main__':
