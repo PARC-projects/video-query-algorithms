8da58981a840e7f287801b083882f87d76973cae
fetorres <frank.home@sbcglobal.net>
2018-05-14 17:36:43 -0700

    unstashed changes after breaking federate_compute_matches

--- src/api/authenticate.py
+++ src/api/authenticate.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import coreapi
 
 
 def authenticate(api_url="http://127.0.0.1/"):
@@ -14,10 +13,6 @@ def authenticate(api_url="http://127.0.0.1/"):
             'password': os.environ['API_CLIENT_PASSWORD']
         })
 
-    auth = coreapi.auth.TokenAuthentication(
-        scheme='Token',
-        token=response.json()['token'],
-        domain=os.path.join(api_url,"docs")
-    )
     # TODO: Consider bubbling up meaningful loggin message on != 200
-    return auth
+    return 'Token {}'.format(response.json()['token'])
+
