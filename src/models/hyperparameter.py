5fefa1208cad252f932e8520dc7a279bff1622ed
fetorres <frank.home@sbcglobal.net>
2018-08-28 11:18:59 -0700

    subtracted small increment from threshold so a roundoff error doesn't remove a clip from being a match

--- src/models/hyperparameter.py
+++ src/models/hyperparameter.py
@@ -61,7 +61,7 @@ class Hyperparameter:
         else:
             weight_optimum, threshold_optimum = self.fine_tune(iw0, ith0, losses)
 
-        self.threshold = threshold_optimum
+        self.threshold = threshold_optimum - 3e-6  # add a small buffer to account for round-off errors
         self.weights = {self.streams[0]: 1.0, self.streams[1]: weight_optimum}
 
     def fine_tune(self, iw0, ith0, losses):
