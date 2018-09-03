344dab89c9dc2f78a4c53eeea8611cff7bc2d3a6
Frank Torres <fetorres@users.noreply.github.com>
2018-09-02 18:13:19 -0700

    hyperparameter mu default - update

--- set_environ.sh
+++ set_environ.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export TSN_ROOT='/data/torres/temporal-segment-networks'
 export TSN_ENVIRON='TSN'
-export API_CLIENT_USERNAME='torres'
-export API_CLIENT_PASSWORD='fetorres'
+export API_CLIENT_USERNAME=''
+export API_CLIENT_PASSWORD=''
 export BROKER_THREADING='True'
 export COMPUTE_EPS=.000003
 export RANDOM_SEED=73459912436
