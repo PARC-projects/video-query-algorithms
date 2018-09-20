6e4c57d34b73a26398837ac227bdadc6dd09ed57
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-09-20 14:07:42 -0500

    Revert "Add UT for hyperparameter class - refasctor to smaller functions"
    This reverts commit 96a6854f56e02589b452aae2403ec66b1252c4ce.

--- src/models/hyperparameter.ut.py
+++ src/models/hyperparameter.ut.py
@@ -6,8 +6,8 @@ class HyperparameterTest(unittest.TestCase):
     """Tests for hyperparameter.py."""
 
     def setUp(self):
-        default_weights = {'rgb': 1.0, 'warped_optical_flow': 1.5}
-        self.hyperparameter = Hyperparameter(default_weights)
+        # default_weights = {'rgb': 1.0, 'warped_optical_flow': 1.5}
+        # self.hyperparameter = Hyperparameter(default_weights)
         pass
 
     def tearDown(self):
@@ -15,18 +15,6 @@ class HyperparameterTest(unittest.TestCase):
 
     def test_true(self):
         self.assertTrue(True)
-
-    def test_add_buffer_threshold_set_to_valid_value(self):
-        # Arrange
-        buffer = 1
-        thresh = 1.00006
-        self.eps_threshold = thresh
-
-        # Act
-        self.hyperparameter.add_buffer(buffer)
-
-        # Assert
-        self.assertEqual(self.eps_threshold, buffer - thresh)
 
 
 if __name__ == '__main__':
